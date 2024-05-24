ba492ab274de19c64fcc22b4c9327b843ec49732
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-24 16:34:31 +0200

    Feat → added trains migration data and launched migration

--- database/migrations/2024_05_24_133028_create_trains_table.php
+++ database/migrations/2024_05_24_133028_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 100);
+            $table->string('departure_station', 200);
+            $table->string('arrival_station', 200);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->unsignedTinyInteger('train_carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false)->nullable();
             $table->timestamps();
         });
     }
