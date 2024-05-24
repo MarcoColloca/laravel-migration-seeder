4cb84f7bfb3dfc36d13a96e99875bf9b609df00a
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-24 17:12:58 +0200

    Refactor → changed indentation in trains View

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -10,27 +10,26 @@
     </section>
     
 
-    <div class="section py-5">
+    <section class="py-5">
         <div class="container">
             <div class="row g-5">
                 @foreach ($trains as $train)
                     @if ($train->departure_time >= $date)                    
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <p><span class="text-danger">Agenzia: </span> {{$train->agency}}</p>
-                                <p><span class="text-danger">Stazione di Partenza: </span>{{$train->departure_station}}</p>
-                                <p><span class="text-danger">Stazione di Arrivo: </span> {{$train->arrival_station}}</p>
-                                <p><span class="text-danger">Orario di Partenza: </span> {{$train->departure_time}}</p>
-                                <p><span class="text-danger">Orario di Arrivo: </span> {{$train->arrival_time}}</p>
-                                <p><span class="text-danger">Carrozze: </span> {{$train->train_carriages}}</p>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <p><span class="text-danger">Agenzia: </span> {{$train->agency}}</p>
+                                    <p><span class="text-danger">Stazione di Partenza: </span>{{$train->departure_station}}</p>
+                                    <p><span class="text-danger">Stazione di Arrivo: </span> {{$train->arrival_station}}</p>
+                                    <p><span class="text-danger">Orario di Partenza: </span> {{$train->departure_time}}</p>
+                                    <p><span class="text-danger">Orario di Arrivo: </span> {{$train->arrival_time}}</p>
+                                    <p><span class="text-danger">Carrozze: </span> {{$train->train_carriages}}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    @endif
-                
+                    @endif                
                 @endforeach
             </div>
         </div>
-    </div>
+    </section>
 @endsection
