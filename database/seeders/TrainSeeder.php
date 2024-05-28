d0a4401ff244af989220d9cf939ada53a8004191
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-28 09:55:08 +0200

    Refactor → moved logic in TrainSeeder into separate function

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -18,6 +18,21 @@ class TrainSeeder extends Seeder
         return [$start, $end];
     }
 
+    protected function isCancelled(Faker $faker){
+
+        $on_time = $faker->randomElement([true, false]);
+
+        if( $on_time === true)
+        {
+            $is_cancelled = false;
+        }else{
+            $is_cancelled = $faker->randomElement([true, false]);
+        };
+
+        return [$on_time, $is_cancelled];
+    }
+
+
     /**
      * Run the database seeds.
      */
@@ -34,28 +49,24 @@ class TrainSeeder extends Seeder
             $new_train = new Train();
             
             list($start, $end) = $this->genTwoCities($faker);
+            list($on_time, $is_cancelled) = $this->isCancelled($faker);
+
             $new_train->departure_station = $start;
             $new_train->arrival_station = $end;
             $new_train->agency = $faker->randomElement($agencies);
             $new_train->departure_time = $faker->dateTimeBetween('-1 month', '+1 month');
             $new_train->arrival_time = $faker->dateTimeInInterval($new_train->departure_time, '+1 day');
             $new_train->train_code = $faker->bothify('??-######');
-            $new_train->train_carriages = $faker->numberBetween(6, 15);
+            $new_train->train_carriages = $faker->numberBetween(6, 15);            
+            $new_train->on_time = $on_time;
+            $new_train->cancelled = $is_cancelled;
             
-            $new_train->on_time = $faker->randomElement([true, false]);
-            if( $new_train->on_time === true)
-            {
-                $new_train->cancelled = false;
-            }else{
-                $new_train->cancelled = $faker->randomElement([true, false]);
-            };
-
 
 
             // Salvo i dati con il medoto save
-            $new_train->save();
+            //$new_train->save();
 
-            //dump($new_train);
+            dump($new_train);
         }
 
     }
